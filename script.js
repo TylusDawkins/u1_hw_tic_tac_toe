37b99e760fdbccbe0788c7b18537e23b24f378a1
Tylus Dawkins <100214696+TylusDawkins@users.noreply.github.com>
2023-11-19 10:26:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,6 @@
-const boxes = document.querySelectorAll('.box')
-const resetButton = document.querySelector('#reset')
-let board = [0,0,0,0,0,0,0,0,0]
-let turns = 0
-const playerX = "X"
-let xArray = []
-const playerO = "O"
-let oArray = []
-winner=false
-isGameActive=true
-
-const findBox = (box) =>{
-    if(turns % 2 == 0){
-        board[box] ="x"
-        console.log(board)
-    }
-    else{
-        board[box] ="o"
-        console.log(board)
-    }
-}
-
-for(let i = 0; i<boxes.length; i++){
-    boxes[i].addEventListener('click', function(){
-        if(isGameActive = true){
-            if(turns % 2 == 0){
-                boxes[i].innerText=playerX
-                turns++;
-                console.log(winner)
-            }
-        
-            else if(turns % 2 == 1){
-                boxes[i].innerText=playerO
-                turns++;
-                console.log(winner)
-        
-        }
-    }
-    })
-}
+const board = document.querySelectorAll('.box')
+let playerTurn = "X"
+let winner = false
 
 const winningConditions = [
     // horizontal win conditions
@@ -53,56 +16,26 @@ const winningConditions = [
     [2, 4, 6]
 ];
 
-
-// const checkWinner = () => {
-
-
-//     const boxValue1 = board[winningConditions[0]]
-//     const boxValue2 = board[winningConditions[1]]
-//     const boxValue3 = board[winningConditions[2]]
-
-//     if (boxValue1 != null && boxValue1 === boxValue2 && boxValue2 === boxValue3) {
-//         gameOverScreen(boxValue1)
-//         winner = true
-//         } 
-//         else {
-//         const drawGame = board.every((box)=> box != null)
-//         if (drawGame) {
-//         gameOverScreen(null)
-//         }
-//     }
-// }
-
-
-// function handleResultValidation() {
-//     let roundWon = false;
-//     for (let i = 0; i <= 7; i++) {
-//         const winCondition = winningConditions[i];
-//         const a = board[winCondition[0]];
-//         const b = board[winCondition[1]];
-//         const c = board[winCondition[2]];
-//         if (a === '' || b === '' || c === '') {
-//             continue;
-//         }
-//         if (a == b && b == c) {
-//             roundWon = true;
-//             winner = true;
-//             break;
-//         }
-//     }
-
-//     if (roundWon) {
-//         announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
-//         isGameActive = false;
-//         return;
-//     }
-
-//     if (!board.includes(''))
-//     announce(TIE);
-// }
-
-const reset = () => {
-    for(let i = 0; i < boxes.length; i++){
-        boxes[i].innerText = null
-    }
+const checkWinner = () => {
+    winningConditions.forEach((condition,i) => {
+        console.log()
+        if(board[condition[0]].innerText !== "" && board[condition[0]].innerText === board[condition[1]].innerText && board[condition[1]].innerText === board[condition[2]].innerText){
+            console.log('Win!')
+            winner = true
+        }
+    })
 }
+
+const play = (e) => {
+    if(winner === true || e.target.innerText != ""){
+        return
+    }
+    e.target.innerText = playerTurn
+    playerTurn = playerTurn === "X" ? "O" : "X" 
+    checkWinner()
+}
+
+board.forEach((box) => {
+    box.addEventListener('click', (e)=>{play(e)})
+})
+
